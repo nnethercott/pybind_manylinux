38315ded84adeaf65ea5b155f8bbac64e0d2cff5
nnethercott <natenethercott@gmail.com>
2022-09-23 18:10:07 +0200

    bro

---
+++ setup.py
@@ -0,0 +1,48 @@
+#TEMPLATE COPIED FROM ONLINE TUTORIAL SUGGESTED BY PYBIND11 DOCS
+#https://github.com/pybind/python_example
+
+import sys
+
+from pybind11 import get_cmake_dir
+# Available at setup time due to pyproject.toml
+try:
+    from pybind11.setup_helpers import Pybind11Extension, build_ext
+except ImportError:
+    from setuptools import Extension as Pybind11Extension, build_ext
+
+
+from setuptools import setup
+
+__version__ = "0.0.1"
+
+# The main interface is through Pybind11Extension.
+# * You can add cxx_std=11/14/17, and then build_ext can be removed.
+# * You can set include_pybind11=false to add the include directory yourself,
+#   say from a submodule.
+#
+# Note:
+#   Sort input source files if you glob sources to ensure bit-for-bit
+#   reproducible builds (https://github.com/pybind/python_example/pull/53)
+
+ext_modules = [
+    Pybind11Extension("nrjn",
+        ["src/main.cpp"],
+        # Example: passing in the version to the compiled code
+        define_macros = [('VERSION_INFO', __version__)],
+        ),
+]
+
+setup(
+    name="nrjn",
+    version=__version__,
+    author="nate nethercott",
+    author_email="natenethercott@gmail.com",
+    description="A test project using pybind11",
+    long_description="",
+    ext_modules=ext_modules,
+    # Currently, build_ext only provides an optional "highest supported C++
+    # level" feature, but in the future it may provide more features.
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.6",
+)
